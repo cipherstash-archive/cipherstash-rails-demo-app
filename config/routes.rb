6e4c16964fc753948de29b29f976c098da884657
Lindsay Holmwood <lindsay@cipherstash.com>
2023-04-12 21:02:22 +1000

    Redirect to the patients admin interface by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/admin', to: redirect('/admin/patients')
   ActiveAdmin.routes(self)
   resources :patients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
