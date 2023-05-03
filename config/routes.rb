eebc42951587e6188a6e7b4bfe08be0d52b200a8
Artem Yakimenko <code@temik.me>
2023-05-03 18:12:57 +1000

    Add a default route to redirect people to demo right from root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root to: redirect('/admin')
+
   get '/admin', to: redirect('/admin/patients')
   ActiveAdmin.routes(self)
   resources :patients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
